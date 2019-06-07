9c7d6d152094408023e59493b47e6d0d6ace2ccc
Ed <215186@edu.rocfriesepoort.nl>
2019-06-07 10:10:36 +0200

    Ordering some files

--- app/src/main/java/com/sarcasm/app/activities/LoginActivity.java
+++ app/src/main/java/com/sarcasm/app/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.sarcasm.app;
+package com.sarcasm.app.activities;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,9 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.sarcasm.app.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
