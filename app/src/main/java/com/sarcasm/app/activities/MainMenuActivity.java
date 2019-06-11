f434d8c3d742ac74c82d42562da5915035976571
Ed <215186@edu.rocfriesepoort.nl>
2019-06-11 10:25:49 +0200

    Main menu activity

---
+++ app/src/main/java/com/sarcasm/app/activities/MainMenuActivity.java
@@ -0,0 +1,23 @@
+package com.sarcasm.app.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.sarcasm.app.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
