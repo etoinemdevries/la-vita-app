8899a51ee371c913c45628c68683f3c528724dda
Robert <robertkremer.frl@gmail.com>
2019-06-12 10:44:43 +0200

    Modified project activities, strings and names

---
+++ app/src/main/java/com/sarcasm/app/activities/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sarcasm.app.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.sarcasm.app.R;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected final void onCreate(final Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+    }
+
+}
