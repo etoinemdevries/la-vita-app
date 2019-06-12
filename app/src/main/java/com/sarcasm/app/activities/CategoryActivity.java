dc7a32dd12fe2100a961dcd1f9f53d8fe9382439
Ed <215186@edu.rocfriesepoort.nl>
2019-06-12 11:00:31 +0200

    Added CategoryActivity

---
+++ app/src/main/java/com/sarcasm/app/activities/CategoryActivity.java
@@ -0,0 +1,14 @@
+package com.sarcasm.app.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sarcasm.app.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
