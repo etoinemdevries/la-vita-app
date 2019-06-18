355258d00af3326c169a2d06e92ac841c612bc83
Ed <215186@edu.rocfriesepoort.nl>
2019-06-18 12:35:39 +0200

    Added code to automatically put text in categoryactivity

--- app/src/main/java/com/sarcasm/app/activities/CategoryActivity.java
+++ app/src/main/java/com/sarcasm/app/activities/CategoryActivity.java
@@ -2,13 +2,33 @@ package com.sarcasm.app.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.sarcasm.app.R;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    private LinearLayout list;
+
+    public String frisdranken[] = {
+            "Cola", "Ice-tea", "7-up", "Cassis", "Sinas"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        this.list = findViewById(R.id.productList);
+        addLayout(frisdranken);
+    }
+
+    private void addLayout(String[] products) {
+        for (int i = 0; i < products.length; i++) {
+            TextView tv = new TextView(this);
+            tv.setText(products[i]);
+            this.list.addView(tv);
+        }
     }
 }
