3db353179d82a8766fb8d3e9d8bb323e0a7a4276
Ed <215186@edu.rocfriesepoort.nl>
2019-06-19 12:21:15 +0200

    CategoryActivity dynamic products start

--- app/src/main/java/com/sarcasm/app/activities/CategoryActivity.java
+++ app/src/main/java/com/sarcasm/app/activities/CategoryActivity.java
@@ -3,19 +3,17 @@ package com.sarcasm.app.activities;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.sarcasm.app.R;
 
 public class CategoryActivity extends AppCompatActivity {
-
     private LinearLayout list;
-
-    public String frisdranken[] = {
-            "Cola", "Ice-tea", "7-up", "Cassis", "Sinas"
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +21,48 @@ public class CategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_category);
 
         this.list = findViewById(R.id.productList);
-        addLayout(frisdranken);
     }
 
-    private void addLayout(String[] products) {
-        for (int i = 0; i < products.length; i++) {
-            TextView tv = new TextView(this);
-            tv.setText(products[i]);
-            tv.setTextColor(Color.parseColor("#009246"));
-            tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-            this.list.addView(tv);
-        }
+    private void addLayout() {
+        final LinearLayout product = new LinearLayout(this);
+        final TextView productInfo = new TextView(this);
+
+        final LinearLayout actionBar = new LinearLayout(this);
+        final ImageView minus = new ImageView(this);
+        final ImageView plus = new ImageView(this);
+        final TextView amount = new TextView(this);
+
+
+        /* Productinfo LinearLayout specs */
+        product.setOrientation(LinearLayout.HORIZONTAL);
+
+        /* Product name + price string */
+        productInfo.setText("Name" + "Price");
+        productInfo.setTextColor(Color.parseColor("#009246"));
+        productInfo.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
+
+        /* Actionbar LinearLayout */
+        actionBar.setGravity(Gravity.END);
+
+        /* Minus image specs */
+        minus.setImageResource(R.drawable.presence_busy);
+
+        /* Plus image specs */
+        plus.setImageResource(R.drawable.ic_input_add);
+
+        /* Amount textview specs */
+        amount.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+
+        /* Add stuff to eachother */
+
+        actionBar.addView(minus);
+        actionBar.addView(amount);
+        actionBar.addView(plus);
+
+        product.addView(productInfo);
+        product.addView(actionBar);
+
+        this.list.addView(product);
     }
 }
