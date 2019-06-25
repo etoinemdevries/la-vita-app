e2c59e2b25e52028fcaf7d364c324613c867e0f8
Ed <215186@edu.rocfriesepoort.nl>
2019-06-25 09:39:12 +0200

    Added getTotalPrice function

--- app/src/main/java/com/sarcasm/app/Receipt.java
+++ app/src/main/java/com/sarcasm/app/Receipt.java
@@ -3,15 +3,23 @@ package com.sarcasm.app;
 import java.util.ArrayList;
 
 public class Receipt {
-    private Integer tableNumber;
-    private ArrayList ids;
+    private final int tableNumber;
+    private final ArrayList<Category.Product> products = new ArrayList<>();
 
     public Receipt(int table) {
         this.tableNumber = table;
-        this.ids = new ArrayList<>();
     }
 
-    public void addId(int id) {
-        this.ids.add(id);
+    public void addProduct(final Category.Product product) {
+        this.products.add(product);
+    }
+
+    public double getTotalPrice() {
+        double price = 0;
+        
+        for (final Category.Product p : this.products)
+            price += p.getPrice();
+
+        return price;
     }
 }
