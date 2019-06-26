38a5f9d52d5e4852036c063c9e95cfc42bfc2cc5
Ed <215186@edu.rocfriesepoort.nl>
2019-06-26 10:48:02 +0200

    Redid the dynamic layout code

--- app/src/main/java/com/sarcasm/app/activities/ReceiptActivity.java
+++ app/src/main/java/com/sarcasm/app/activities/ReceiptActivity.java
@@ -32,53 +32,23 @@ public class ReceiptActivity extends AppCompatActivity {
         final Receipt receipt = MainActivity.receipt;
 
         /* Add all products */
-        /* Sorting of products TODO */
-        //final ArrayList<Category.Product> values = Category.Product.values();
-
         for(final Category.Product p : Category.Product.values()){
             final int count = receipt.getAmount(p);
             if (count <= 0) continue;
 
-            addProduct(p, count, point.x, point.y);
+            addLayout(p.getName(), getPrice((p.getPrice() * count)), count, point.x, point.y);
         }
+
+        /* Add total price section */
+        addLayout("Totaal", getPrice(receipt.getTotalPrice()), 0, point.x, point.y);
     }
 
     /** Adds a product to the interface */
-    private final void addProduct(final Category.Product p, final int count, final int width, int value){
-        final LinearLayout layout = new LinearLayout(this);
-        final TextView price = new TextView(this);
-        final TextView text = new TextView(this);
+    private final void addLayout(final String textString, final String priceString, final int count, final int width, int value){
         value /= 6;
-
-        /* Set layout specs */
-        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        layout.setOrientation(LinearLayout.HORIZONTAL);
-        layout.setMinimumWidth(width);
-        layout.setMinimumHeight(value);
-
-        /* Set price specs */
-        final double money = p.getPrice();
-
-        price.setText(getPrice(money * count));
-        price.setTextColor(Color.parseColor("#009246"));
-        price.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
-
-        price.setGravity(Gravity.CENTER_VERTICAL);
-        price.setX(5);
-        price.setWidth(value);
-        price.setHeight(value);
-
-        /* Set text specs */
-        text.setTextColor(Color.parseColor("#009246"));
-        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
-
-        if(count > 1) text.setText(p.getName() + " (x" + count + ")");
-        else text.setText(p.getName());
-
-        text.setGravity(Gravity.CENTER_VERTICAL);
-        text.setX(value / 2);
-        text.setWidth(value * 2);
-        text.setHeight(value);
+        final LinearLayout layout = makeLayout(width, value);
+        final TextView price = makePrice(priceString, value);
+        final TextView text = makeText(textString, count, value);
 
         /* Add everything to layouts */
         layout.addView(text);
@@ -87,8 +57,52 @@ public class ReceiptActivity extends AppCompatActivity {
         this.list.addView(layout);
     }
 
+    private final LinearLayout makeLayout(final int width, int value) {
+        final LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setMinimumWidth(width);
+        layout.setMinimumHeight(value);
+
+        return layout;
+    }
+
+    private final TextView makeText(final String textString, final int count, int value) {
+        final TextView text = new TextView(this);
+
+        text.setTextColor(Color.parseColor("#009246"));
+        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+
+        if(count > 1) text.setText(textString + " (x" + count + ")");
+        else {
+            text.setTextColor(Color.BLACK);
+            text.setText(textString);
+        }
+
+        text.setGravity(Gravity.CENTER_VERTICAL);
+        text.setX(value / 2);
+        text.setWidth(value * 2);
+        text.setHeight(value);
+
+        return text;
+    }
+
+    private final TextView makePrice(final String money, int value) {
+        final TextView price = new TextView(this);
+        price.setText(money);
+        price.setTextColor(Color.parseColor("#009246"));
+        price.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
+
+        price.setGravity(Gravity.CENTER_VERTICAL);
+        price.setX(5);
+        price.setWidth(value);
+        price.setHeight(value);
+
+        return price;
+    }
+
     /** Gets the price of a product as a string */
-    public final String getPrice(final double price){
+    private final String getPrice(final double price){
         final int rounded = (int)price;
         final double decimal = price - rounded;
 
