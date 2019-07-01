81f31dbf13ced99fc899b88260e75361c0207729
Robert <robertkremer.frl@gmail.com>
2019-07-01 11:42:50 +0200

    Connection to wenhao hotspot

--- app/src/main/java/com/sarcasm/app/utils/Encryption.java
+++ app/src/main/java/com/sarcasm/app/utils/Encryption.java
@@ -8,18 +8,12 @@ public final class Encryption {
 
     /** Encrypts a string */
     public static final String encrypt(final String input){
-        return new String(Base64.encode(input.getBytes(getCharset()), Base64.DEFAULT));
+        return new String(Base64.encode(input.getBytes(), Base64.DEFAULT));
     }
 
     /** Decrypts a string */
     public static final String decrypt(final String input){
         final String base64 = new String(Base64.decode(input, Base64.DEFAULT));
-        return new String(base64.getBytes(getCharset()));
-    }
-
-    /** Gets the charset used for encrypting and decrypting */
-    public static final Charset getCharset() {
-        if(Charset.isSupported("UTF32")) return Charset.forName("UTF32");
-        return Charset.defaultCharset();
+        return new String(base64.getBytes());
     }
 }
